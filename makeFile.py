9bfb0b898951a5c3ba56cc45d9f5644f97850f05
Hapache <hapache_chen@gmail.com>
2020-05-20 16:21:40 +0800

    makeFile 新增註解，刪除一些 Dirty Code MangoTest 是測試程式碼用的檔案，不需要

--- makeFile.py
+++ makeFile.py
@@ -1,10 +1,12 @@
-import os
 import tensorflow as tf
-from PIL import Image
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pds
 import IPython.display as display
+
+# Create a description of the features.
+feature_description = {
+    'label': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+    'img_raw': tf.io.FixedLenFeature([], tf.string, default_value=''),
+}
 
 def _int64_feature(value):
     return tf.train.Feature(int64_list = tf.train.Int64List(value = [value]))
@@ -15,14 +17,13 @@ def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 def make_dataset(fileName, folderName):
+    # 讀取 csv 檔， csv 存放每張圖片的分類等級
     fileTrain = pds.read_csv(fileName + '.csv', encoding='big5')
-    writer = tf.io.TFRecordWriter('train.tfrecords') #輸出成tfrecord檔案
+    # TFRecord 的檔名
+    writer = tf.io.TFRecordWriter('train.tfrecords')
 
     for i in range(fileTrain.shape[0]):
-        img_path = folderName + '/' + fileTrain.iloc[i, 0] #每個圖片的地址
-        #img = Image.open(img_path)
-        #img = img.resize((208, 208))
-        #img_raw = img.tobytes()  #將圖片轉化為二進位制格式
+        # 將圖片讀成二進制
         img_raw = open(folderName + '/' + fileTrain.iloc[i, 0], 'rb').read()
         if fileTrain.iloc[i, 1].upper() == 'A':
             label = 0
@@ -30,26 +31,25 @@ def make_dataset(fileName, folderName):
             label = 1
         elif fileTrain.iloc[i, 1].upper() == 'C':
             label = 2
+        # 每個 Example 含有 label 、 img_raw 兩個資訊。
         example = tf.train.Example(features = tf.train.Features(feature = {
             "label": _int64_feature(label),
             "img_raw": _bytes_feature(img_raw)
         }))
-        writer.write(example.SerializeToString())  #序列化為字串
+        # 序列化為字串
+        writer.write(example.SerializeToString())
     writer.close()
-
-# Create a description of the features.
-feature_description = {
-    'label': tf.io.FixedLenFeature([], tf.int64, default_value=0),
-    'img_raw': tf.io.FixedLenFeature([], tf.string, default_value=''),
-}
 
 def _parse_function(example_proto):
     # Parse the input `tf.Example` proto using the dictionary above.
     return tf.io.parse_single_example(example_proto, feature_description)
 
-def read_and_decode(filename, batch_size): # read train.tfrecords
-    filename_queue = tf.data.TFRecordDataset(filename)# create a queue
-    parsed_image_dataset = filename_queue.map(_parse_function)
+def read_and_decode(filename):
+    # 讀入 TFRecords 檔
+    file = tf.data.TFRecordDataset(filename)
+    # Parse Data
+    parsed_image_dataset = file.map(_parse_function)
+    # 顯示圖片
     for image_features in parsed_image_dataset:
         image_raw = image_features['img_raw'].numpy()
         display.display(display.Image(data = image_raw))
@@ -57,7 +57,4 @@ def read_and_decode(filename, batch_size): # read train.tfrecords
 fileName = 'label'
 folderName = 'sample_image'
 #make_dataset(fileName, folderName)
-
-read_and_decode('train.tfrecords', 32)
-
-#show_image()
+read_and_decode('train.tfrecords')
