c016cc96001a22867cc6561b25d9c3e53f8eb0dc
Hapache <hapache_chen@gmail.com>
2020-06-12 14:51:56 +0800

    自動依照 Label 分資料夾

--- moveFile.py
+++ moveFile.py
@@ -7,17 +7,13 @@ def splitImage(fileName, folderName):
     if not os.path.isdir(fileName):
         os.mkdir(fileName)
         print(f'Create folder {fileName}')
-    if not os.path.isdir(fileName + "/A"):
-        os.mkdir(fileName + "/A")
-        print(f'Create folder {fileName + "/A"}')
-    if not os.path.isdir(fileName + "/B"):
-        os.mkdir(fileName + "/B")
-        print(f'Create folder {fileName + "/B"}')
-    if not os.path.isdir(fileName + "/C"):
-        os.mkdir(fileName + "/C")
-        print(f'Create folder {fileName + "/C"}')
     for i in range(file.shape[0]):
+        if not os.path.isdir(fileName + "/" + file.iloc[i][1]):
+            os.mkdir(fileName + "/" + file.iloc[i][1])
+            print(f'Create folder {fileName + "/" + file.iloc[i][1]}')
+        # 取得檔案
         originalPath = folderName + "/" + file.iloc[i][0]
+        # 分到該 Label 目錄下
         newPath = fileName + "/" + file.iloc[i][1]
         shutil.copy(originalPath, newPath)
 
